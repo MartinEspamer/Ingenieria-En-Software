a5d2f529de4b8a2a57c7afe4df15620eabe6fdc7
Martin Espamer <martinespamer@hotmail.com>
2025-04-11 13:46:24 -0300

    [Parte 2.2] Creando modelos, migrando cambios e incluyendo app en MySite[D

--- MySite/encuestas/models.py
+++ MySite/encuestas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Preguntas(models.Model):
+	texto_preguntas = models.CharField(max_length=200)
+	fecha_publicacion = models.DateTimeField("fecha publicacion")
+
+class Eleccion(models.Model):
+	pregunta = models.ForeignKey ( Preguntas, on_delete=models.CASCADE )
+	texto_elegido = models.CharField(max_length=200)
+	votos = models.IntegerField(default=0)
