9c016fb2c411aac0690b0e52306699b5e29cfd37
martinespamer <martinespamer@alu.frlp.utn.edu.ar>
2025-04-12 00:16:50 -0300

    [Parte 3.1] Desarrollo de vistas de prueba

--- MySite/encuestas/urls.py
+++ MySite/encuestas/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path("", views.index ,name="index"),
+    # ej: /encuestas/
+    path("", views.index, name="index"),
+    # ex: /encuestas/5/
+    path("<int:pregunta_id>/", views.detalle, name="detalles"),
+    # ex: /encuestas/5/resultados/
+    path("<int:pregunta_id>/results/", views.resultados, name="resultados"),
+    # ex: /encuestas/5/votos/
+    path("<int:pregunta_id>/votos/", views.voto, name="votos"),	
 ]
--- MySite/encuestas/views.py
+++ MySite/encuestas/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola mundo, Estamos en el index de encuestas")
+
+def detalle(request, pregunta_id):
+    return HttpResponse("estas viendo la pregunta %s." %pregunta_id)
+
+def resultados(request, pregunta_id):
+    response = "estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % pregunta_id)
+
+def voto(request, pregunta_id):
+    return HttpResponse("estas votando la pregunta %s." % pregunta_id)
+
