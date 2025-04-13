66855732234ca31e1ffd7ce5f1614c489fc81b28
Martin Espamer <martinespamer@hotmail.com>
2025-04-13 13:34:03 -0300

    [Parte 4.1] Escribiendo un minimal form

--- MySite/encuestas/views.py
+++ MySite/encuestas/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Preguntas, Eleccion
 from  django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 def index(request,pregunta_id):
     pregunta = get_object_or_404(Preguntas, pk=pregunta_id) #obtiene la pregunta por su id
     return render(request, 'encuestas/index.html', {"pregunta":pregunta}) #renderiza la plantilla index.html
@@ -10,9 +11,20 @@ def detalle(request, pregunta_id):
     return HttpResponse("estas viendo la pregunta %s." %pregunta_id)
 
 def resultados(request, pregunta_id):
-    response = "estas viendo los resultados de la pregunta %s."
-    return HttpResponse(response % pregunta_id)
-
+    pregunta = get_object_or_404(Preguntas, pk=pregunta_id) #obtiene la pregunta por su id
+    return render(request, 'encuestas/resultados.html', {"pregunta":pregunta}) #renderiza la plantilla resultados.html
+    
 def voto(request, pregunta_id):
-    return HttpResponse("estas votando la pregunta %s." % pregunta_id)
+    pregunta = get_object_or_404(Preguntas, pk=pregunta_id) #obtiene la pregunta por su id
+    try:
+        seleccion = pregunta.eleccion_set.get(pk=request.POST["eleccion"]) #obtiene la eleccion por su id
+    except (KeyError, Eleccion.DoesNotExist):
+        return render(request, 'encuestas/index.html', {
+            'pregunta':pregunta,
+            'error_message':"No seleccionaste una opcion.",
+        })
+    else:
+        seleccion.votos += 1 #incrementa el voto
+        seleccion.save() #guarda el voto en la base de datos
+        return HttpResponseRedirect(reverse('encuestas:resultados', args=(pregunta.id,))) #redirecciona a la vista resultados
 
